783f414d965d1cd285fba4950108ac6cfbd1f278
annrok20 <ania-rokicka@wp.pl>
2017-12-03 22:41:41 +0100

    Add files via upload

---
+++ lab6/abc/test/Figura/TestTest.java
@@ -0,0 +1,13 @@
+package Figura;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestTest {
+    
+    
+}
