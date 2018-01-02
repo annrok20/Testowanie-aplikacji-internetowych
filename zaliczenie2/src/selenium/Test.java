d2e80e7432b403a65030dbc9b71a78ff2b8c5bb7
annrok20 <ania-rokicka@wp.pl>
2018-01-02 22:41:13 +0100

    Add files via upload
    Zaliczenie z Selenium

---
+++ zaliczenie2/src/selenium/Test.java
@@ -0,0 +1,16 @@
+package selenium;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Test {
+	@Before
+	public void setUp() {
+		System.setProperty("webdriver.gecko.driver", "C:/Users/Ana/Desktop/infa1718/testowanie/chromedriver.exe");
+	}
+	
+	@Test
+	public void testPage(){
+		
+	}
+}
